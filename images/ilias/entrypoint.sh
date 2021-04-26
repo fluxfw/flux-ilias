88e16d46161d1b348444d0f21f00fb1623a80b61
fluxfw <fluxfw@fluxlabs.ch>
2021-04-26 07:54:40 +0200

    Use composer2 for ILIAS 7 and higher

--- images/ilias/entrypoint.sh
+++ images/ilias/entrypoint.sh
@@ -86,14 +86,20 @@ memory_limit = $ILIAS_PHP_MEMORY_LIMIT
 post_max_size = $ILIAS_PHP_POST_MAX_SIZE
 upload_max_filesize = $ILIAS_PHP_UPLOAD_MAX_SIZE" > "$PHP_INI_DIR/conf.d/ilias.ini"
 
+if [ ! -d "$ILIAS_WEB_DIR/setup/templates" ]; then
+  composer=composer2
+else
+  composer=composer1
+fi
+
   if [ -f "$ILIAS_WEB_DIR/composer.json" ]; then
     echo "Install composer dependencies"
     case $PHP_VERSION in
       8.*)
-        composer install -d "$ILIAS_WEB_DIR" --no-dev --ignore-platform-reqs
+        $composer install -d "$ILIAS_WEB_DIR" --no-dev --ignore-platform-reqs
       ;;
       *)
-        composer install -d "$ILIAS_WEB_DIR" --no-dev
+        $composer install -d "$ILIAS_WEB_DIR" --no-dev
       ;;
     esac
 
